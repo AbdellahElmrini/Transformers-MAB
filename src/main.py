7c981fe4f5bbe3be0523c376d5c88bc7b0833eb6
Abdellah <abdellah-elmrini@outlook.com>
2023-01-17 09:10:03 +0100

    Added main

--- src/main.py
+++ src/main.py
@@ -10,33 +10,41 @@ import time
 
 from layers import Block
 from config import LightConfig, TransformerConfig, TrainConfig
-from multiarmedbandits import MAB_normal
+from multiarmedbandits import MAB_normal, MAB_normal2, MAB_Bernoulli, MAB_normal_random_order
 from model import UCBTransformerModel
 from dataset import UCB_dataset
-
 
 def train(model, train_loader, train_config : TrainConfig):
     model.train()
     optimizer = Adam(model.parameters(), lr = train_config.lr)
     start = time.time()
     epochs = train_config.epochs
+    train_loss = 0
+    count = 0
     for epoch in range(epochs):
+        train_loss = 0
+        count = 0
         for actions, rewards in train_loader:
             optimizer.zero_grad()
             loss, logits = model(actions, rewards)
             loss.backward()
+            train_loss += loss*actions.shape[0]
             optimizer.step()
+            count += actions.shape[0]
         if epoch%2 == 1:
             tpe = (time.time()-start)/2
-            print("Epoch : {} - Loss : {} - Time per epoch {:.2f}s".format(epoch, loss, tpe))
+            print("Epoch : {} - Loss : {} - Train_Loss : {} - Time per epoch {:.2f}s".format(
+                epoch, loss, train_loss/count, tpe))
             start = time.time()
+
+
 
 if __name__ == "__main__":
 
 
     # Building the datasets
     vocab_size = 5
-    mab1 = MAB_normal(5)
+    mab1 = MAB_normal_random_order(5)
     train_data = UCB_dataset(mab1, 10000, 10)
     #test_data = UCB_dataset(mab1, 1000, 10)
 
